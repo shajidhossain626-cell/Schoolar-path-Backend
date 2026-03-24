2d3846e24a71d3df23061a856d0f211ecb2811e9
shajidhossain626-cell <shajidhossain626@gmail.com>
2026-03-25 03:42:20 +0600

    fix database.js for prisma v7

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,6 @@
 const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
   log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
 })
 
